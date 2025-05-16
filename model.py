d3e60c096116ddbf1b539626db8981b4c6ec1c79
d12o6aa <doaakarem798@gmail.com>
2025-05-16 04:48:49 +0300

    update

--- model.py
+++ model.py
@@ -15,9 +15,7 @@ from datetime import datetime
 import numpy as np
 
 app = Flask(__name__)
-@app.route('/health')
-def health():
-    return "OK", 200
+
 executor = ThreadPoolExecutor()
 
 frame = None
